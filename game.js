233eec053fe9268fde3cedc3de19cea768de2581
Robert Brush <robertcawbrush@gmail.com>
2015-09-16 01:23:09 +0000

    revisted size of enemy, refactored explosion code

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ BasicGame.Game.prototype = {
   preload: function () {
     this.load.image('sea', 'assets/sea.png');
     this.load.image('bullet', 'assets/bullet.png');
-    this.load.spritesheet('smallEnemy', 'assets/enemy.png', 32, 32);
+    this.load.spritesheet('smallEnemy', 'assets/enemy.png', 42, 42);
     this.load.spritesheet('explosion', 'assets/explosion.png', 32, 32);
     this.load.spritesheet('player', 'assets/player.png', 64, 64);
   },
@@ -25,13 +25,14 @@ BasicGame.Game.prototype = {
     this.physics.enable(this.player, Phaser.Physics.ARCADE);
     this.player.speed = 300;
     this.player.body.collideWorldBounds = true;
+    this.player.body.setSize(20, 20, 0, -5);
     
     //create and set smallPlane Bodies
     this.enemyPool = this.add.group();
     this.enemyPool.enableBody = true;
     this.enemyPool.physicsBodyType = Phaser.Physics.ARCADE;
     
-    this.enemyPool.createMultiple (5, 'smallEnemy');
+    this.enemyPool.createMultiple (15, 'smallEnemy');
     this.enemyPool.setAll('anchor.x', 0.5);
     this.enemyPool.setAll('anchor.y', 0.5);
     this.enemyPool.setAll('outOfBoundsKill', true);
@@ -46,25 +47,52 @@ BasicGame.Game.prototype = {
     this.enemyDelay = 100;
     
     //spritegroup
-    this.bulletPool = this.add.group();
+    this.bulletPool1 = this.add.group();
     
     //enable bullet physics
-    this.bulletPool.enableBody = true;
-    this.bulletPool.physicsBodyType = Phaser.Physics.ARCADE;
+    this.bulletPool1.enableBody = true;
+    this.bulletPool1.physicsBodyType = Phaser.Physics.ARCADE;
     
     //create 'magazine'
-    this.bulletPool.createMultiple(100, 'bullet');
+    this.bulletPool1.createMultiple(100, 'bullet');
     
-    this.bulletPool.setAll('anchor.x', 0.5);
-    this.bulletPool.setAll('anchor.y', 0.5);
+    this.bulletPool1.setAll('anchor.x', 0.5);
+    this.bulletPool1.setAll('anchor.y', 0.5);
     
     //destroys bullets if out of world view
-    this.bulletPool.setAll('outOfBoundsKill', true);
-    this.bulletPool.setAll('checkWorldBounds', true);
+    this.bulletPool1.setAll('outOfBoundsKill', true);
+    this.bulletPool1.setAll('checkWorldBounds', true);
+    
+    //spritegroup
+    this.bulletPool2 = this.add.group();
+    
+    //enable bullet physics
+    this.bulletPool2.enableBody = true;
+    this.bulletPool2.physicsBodyType = Phaser.Physics.ARCADE;
+    
+    //create 'magazine'
+    this.bulletPool2.createMultiple(100, 'bullet');
+    
+    this.bulletPool2.setAll('anchor.x', 0.5);
+    this.bulletPool2.setAll('anchor.y', 0.5);
+    
+    //destroys bullets if out of world view
+    this.bulletPool2.setAll('outOfBoundsKill', true);
+    this.bulletPool2.setAll('checkWorldBounds', true);
     
     //establishes firing rate
     this.nextShotAt = 0;
-    this.shotDelay = 150;
+    this.shotDelay = 130;
+    
+    this.explosionPool = this.add.group();
+    this.explosionPool.enableBody = true;
+    this.explosionPool.physicsBodyType = Phaser.Physics.ARCADE;
+    this.explosionPool.createMultiple(100,'explosion');
+    this.explosionPool.setAll('anchor.x', 0.5);
+    this.explosionPool.setAll('anchor.y', 0.5);
+    this.explosionPool.forEach(function (explosion){
+      explosion.animations.add('boom');
+    });
     
     //input inits
       //arrow keys
@@ -76,10 +104,13 @@ BasicGame.Game.prototype = {
 
   update: function () {
     //scroll map
-    this.sea.tilePosition.y += 1;
+    this.sea.tilePosition.y += 0.2;
     
     //bullet collision check
-    this.physics.arcade.overlap(this.bulletPool, this.enemyPool, this.enemyHit, null, this);
+    this.physics.arcade.overlap(this.bulletPool1, this.enemyPool, this.enemyHit, null, this);
+    
+    //player collision check
+    this.physics.arcade.overlap(this.player, this.enemy, this.playerHit, null, this);
     
     //enemySpawn set to random
     if (this.nextEnemyAt < this.time.now && this.enemyPool.countDead() > 0) {
@@ -116,16 +147,13 @@ BasicGame.Game.prototype = {
           
           this.fire();
     }
-   
-    
   },
   
   render: function() {
+    this.game.debug.body(this.player);
   },
 
   quitGame: function (pointer) {
-
-
 
     //  Then let's go back to the main menu.
     this.state.start('MainMenu');
@@ -134,32 +162,49 @@ BasicGame.Game.prototype = {
 
   enemyHit: function(bullet, enemy) {
     bullet.kill();
+    this.explode(enemy);
     enemy.kill();
     
-    var explosion = this.add.sprite(enemy.x, enemy.y, 'explosion');
-    explosion.anchor.setTo(0.5, 0.5);
-    explosion.animations.add('boom');
-    explosion.play('boom', 15,false, true);
   },
   
   fire: function() {
     //delay shot so firing rate isn't insane
-    if(this.nextShotAt > this.time.now) {
+    if(!this.player.alive || this.nextShotAt > this.time.now) {
       return;
     }
     
-    if(this.bulletPool.countDead() === 0) {
+    if(this.bulletPool1.countDead() === 0 && this.bulletPool2 === 0) {
       return;
     }
     
     this.nextShotAt = this.time.now + this.shotDelay;
      
-    var bullet = this.bulletPool.getFirstExists(false);
+    var bullet1 = this.bulletPool1.getFirstExists(false);
+    var bullet2 = this.bulletPool2.getFirstExists(false);
     
-    bullet.reset(this.player.x, this.player.y -20);
+    bullet1.reset(this.player.x -17, this.player.y -20);
+    bullet2.reset(this.player.x +17, this.player.y -20);
     
-    bullet.body.velocity.y = -500;
+    bullet1.body.velocity.y = -500;
+    bullet2.body.velocity.y = -500;
     
+ },
+ 
+ playerHit: function (player, enemy){
+   enemy.kill();
+   this.explode(enemy);
+   player.kill();
+ },
+ 
+ explode: function (sprite) {
+   if (this.explosionPool.countDead() === 0){
+     return;
+   } 
+   var explosion = this.explosionPool.getFirstExists(false);
+   explosion.reset(sprite.x, sprite.y);
+   explosion.play('boom', 15, false, true);
+   explosion.body.velocity.y = sprite.body.velocity.y;
+   explosion.body.velocity.x = sprite.body.velocity.x;
  }
 
 };
